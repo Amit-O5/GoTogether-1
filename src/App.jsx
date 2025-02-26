3702a838132b1bf5eb66ddc22ea571d9646651b5
Vipul Beniwal <120404187+vipulbeniwal01@users.noreply.github.com>
2025-02-26 21:11:36 +0530

    code

--- src/App.jsx
+++ src/App.jsx
@@ -20,13 +20,13 @@ function App() {
           <Navbar />
           <main className="container mx-auto px-4 py-8">
             <Routes>
-              <Route path="/" element={<RidesList />} />
+              {/* <Route path="/" element={<RidesList />} />
               <Route
                 path="/rides"
                 element={
                     <RidesList />
                 }
-              />
+              /> */}
               <Route
                 path="/rides/create"
                 element={
@@ -59,6 +59,8 @@ function App() {
                   </PrivateRoute>
                 }
               />
+              <Route path="/" element={<RidesList />} />
+              <Route path="/rides" element={<RidesList />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
             </Routes>
